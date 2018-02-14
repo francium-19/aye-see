f05269683ea585c098efd165d02c09d31a624e04
Eric Carrillo <eric.rudolph.carrillo@gmail.com>
2018-02-14 11:45:08 -0800

    front end fixes (#1)
    * user creator and post creator working
    
    * initial, post creator working
    
    * user creator and post creator working
    
    * initial, post creator working
    
    * working front end

--- src/client/containers/HomePageContainer.jsx
+++ src/client/containers/HomePageContainer.jsx
@@ -5,7 +5,7 @@ import PostCreator from '../components/PostCreator.jsx';
 
 class HomePageContainer extends Component {
     constructor(props) {
-      super(props);
+        super(props);
         this.state = {};
         this.state.postList = [];
 
